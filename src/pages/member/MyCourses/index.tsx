db1b888d3e49fa4b460fce3125cfd7e44101cd27
Abdul Latif Mubasir <bj.angel119@gmail.com>
2022-06-27 20:21:29 +0700

    feat: create page my course on the dashboard member

---
+++ src/pages/member/MyCourses/index.tsx
@@ -0,0 +1,63 @@
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import React from "react";
+import CardCourseMember from "../../../components/dashboard/CardCourseMember";
+
+const MyCourses: React.FC = () => {
+  return (
+    <Box>
+      <Box>
+        <Typography variant="h3">Kelasku</Typography>
+        <Grid container spacing={5} sx={{ marginTop: "0px" }}>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ marginTop: "80px" }}>
+        <Typography variant="h3">Kelas Populer</Typography>
+        <Stack direction="row" spacing={3} sx={{ marginTop: "20px" }}>
+          <Button color="secondary">Pemula</Button>
+          <Button color="secondary">Menengah</Button>
+          <Button color="secondary">Professional</Button>
+        </Stack>
+        <Grid container spacing={5} sx={{ marginTop: "0px" }}>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+          <Grid item md={3}>
+            <CardCourseMember />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default MyCourses;
--- src/pages/member/index.tsx
+++ src/pages/member/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MemberLayout from "../../layouts/dashboard/MemberLayout";
 import Dashboard from "./Dashboard";
+import MyCourses from "./MyCourses";
 
 const Member: React.FC = () => {
   return (
     <MemberLayout>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/courses" element={<MyCourses />} />
       </Routes>
     </MemberLayout>
   );
