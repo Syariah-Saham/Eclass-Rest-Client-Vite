1f5ef7e02f48acc79052ae6d6a374d2392cedb90
Abdul Latif Mubasir <bj.angel119@gmail.com>
2022-06-27 12:49:25 +0700

    fix: remove header on the dashboard member

--- src/layouts/dashboard/MemberLayout.tsx
+++ src/layouts/dashboard/MemberLayout.tsx
@@ -112,92 +112,92 @@ const MemberLayout: React.FC<PropsWithChildren> = (props) => {
     <Box sx={{ height: "100vh" }}>
       <Sidebar menus={menus} />
       <Content>
-        <Stack direction="row" justifyContent="space-between">
-          <Typography variant="h4">{dashboard.title}</Typography>
-          <Stack direction="row" justifyContent={"flex-end"} gap={1}>
-            <Box>
-              <IconButton color="secondary" size="large">
-                <NotificationsOutlinedIcon />
-              </IconButton>
-            </Box>
-            <Box>
-              <IconButton color="secondary" size="large">
-                <ShoppingCartOutlinedIcon />
-              </IconButton>
-            </Box>
-            <Box>
-              <Avatar
-                alt="User 1"
-                src="https://i.pinimg.com/564x/f6/c3/79/f6c379ccfb9130cfd36c63722d5251be.jpg"
-                onClick={openMenuProfile}
-                sx={{
-                  cursor: "pointer",
-                  transition: ".25s",
-                  ":hover": {
-                    boxShadow: `0px 0px 0px 2px ${theme.palette.secondary.main}`,
+        {/* <Stack direction="row" justifyContent="space-between"> */}
+        {/* <Typography variant="h4">{dashboard.title}</Typography> */}
+        <Stack direction="row" justifyContent={"flex-end"} gap={1}>
+          <Box>
+            <IconButton color="secondary" size="large">
+              <NotificationsOutlinedIcon />
+            </IconButton>
+          </Box>
+          <Box>
+            <IconButton color="secondary" size="large">
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+          </Box>
+          <Box>
+            <Avatar
+              alt="User 1"
+              src="https://i.pinimg.com/564x/f6/c3/79/f6c379ccfb9130cfd36c63722d5251be.jpg"
+              onClick={openMenuProfile}
+              sx={{
+                cursor: "pointer",
+                transition: ".25s",
+                ":hover": {
+                  boxShadow: `0px 0px 0px 2px ${theme.palette.secondary.main}`,
+                },
+              }}
+            />
+            <Menu
+              open={menuProfile.status}
+              anchorEl={menuProfile.anchorEl}
+              onClose={closeMenuProfile}
+              onClick={closeMenuProfile}
+              PaperProps={{
+                elevation: 1,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
                   },
-                }}
-              />
-              <Menu
-                open={menuProfile.status}
-                anchorEl={menuProfile.anchorEl}
-                onClose={closeMenuProfile}
-                onClick={closeMenuProfile}
-                PaperProps={{
-                  elevation: 1,
-                  sx: {
-                    overflow: "visible",
-                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    "&:before": {
-                      content: '""',
-                      display: "block",
-                      position: "absolute",
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: "background.paper",
-                      transform: "translateY(-50%) rotate(45deg)",
-                      zIndex: 0,
-                    },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
                   },
-                }}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-              >
-                <Link to="/admin/profile">
-                  <MenuItem
-                    onClick={redirectToProfile}
-                    sx={{ padding: "10px 40px" }}
-                  >
-                    <ListItemIcon>
-                      <AccountCircleRounded color="inherit" fontSize="small" />
-                    </ListItemIcon>
-                    Profile
-                  </MenuItem>
-                </Link>
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <Link to="/admin/profile">
                 <MenuItem
-                  onClick={handleLogout}
-                  sx={{ padding: "10px 40px", color: theme.palette.error.main }}
+                  onClick={redirectToProfile}
+                  sx={{ padding: "10px 40px" }}
                 >
-                  <ListItemIcon sx={{ color: theme.palette.error.main }}>
-                    <Logout color="inherit" fontSize="small" />
+                  <ListItemIcon>
+                    <AccountCircleRounded color="inherit" fontSize="small" />
                   </ListItemIcon>
-                  <Typography variant="h6" color="inherit">
-                    Keluar
-                  </Typography>
+                  Profile
                 </MenuItem>
-              </Menu>
-            </Box>
-          </Stack>
+              </Link>
+              <MenuItem
+                onClick={handleLogout}
+                sx={{ padding: "10px 40px", color: theme.palette.error.main }}
+              >
+                <ListItemIcon sx={{ color: theme.palette.error.main }}>
+                  <Logout color="inherit" fontSize="small" />
+                </ListItemIcon>
+                <Typography variant="h6" color="inherit">
+                  Keluar
+                </Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
         </Stack>
+        {/* </Stack> */}
       </Content>
     </Box>
   );
