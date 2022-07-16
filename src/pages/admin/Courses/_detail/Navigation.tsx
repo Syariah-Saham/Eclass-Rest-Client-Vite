84d20b13a768396a88acc98987b81b394a49ecb0
Abdul Latif Mubasir <bj.angel119@gmail.com>
2022-07-16 11:55:24 +0700

    feat: can delete course

--- src/pages/admin/Courses/_detail/Navigation.tsx
+++ src/pages/admin/Courses/_detail/Navigation.tsx
@@ -1,14 +1,65 @@
 import { Stack, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ModalDelete from "../../../../components/modals/ModalDelete";
+import { openSnackbar } from "../../../../redux/actions/snackbar";
+import { useAppDispatch } from "../../../../redux/hooks";
+import { deleteCourse } from "../../../../services/courses";
 
 const Navigation: React.FC = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [modalDelete, setModalDelete] = useState({
+    show: false,
+    onClose: () => {
+      setModalDelete({ ...modalDelete, show: false });
+    },
+    onDelete: () => {},
+  });
+
+  const handleDelete = async () => {
+    setModalDelete({
+      ...modalDelete,
+      show: true,
+      onDelete: async () => {
+        try {
+          await deleteCourse({ id: parseInt(id as string) });
+          dispatch(
+            openSnackbar({
+              severity: "success",
+              message: "Delete course successfully",
+            })
+          );
+          navigate("/admin/courses");
+        } catch (error: any) {
+          dispatch(
+            openSnackbar({
+              severity: "error",
+              message: error?.message,
+            })
+          );
+        }
+      },
+    });
+  };
+
   return (
-    <Stack direction="row" gap={3} sx={{ marginBottom: "30px" }}>
-      <Button color="success">Edit</Button>
-      <Button color="error">Hapus</Button>
-      <Button color="secondary">Publish</Button>
-      <Button color="warning">Ujian</Button>
-    </Stack>
+    <>
+      <Stack direction="row" gap={3} sx={{ marginBottom: "30px" }}>
+        <Button color="success">Edit</Button>
+        <Button color="error" onClick={handleDelete.bind(null, id)}>
+          Hapus
+        </Button>
+        <Button color="secondary">Publish</Button>
+        <Button color="warning">Ujian</Button>
+      </Stack>
+      <ModalDelete
+        show={modalDelete.show}
+        onClose={modalDelete.onClose}
+        onDelete={modalDelete.onDelete}
+      />
+    </>
   );
 };
 
